ad3c1423475da18dffcc663660cc1829c98606ab
haeseoklee <haeseoklee.dev@gmail.com>
2020-11-13 16:28:15 +0900

    gitignore 추가

--- HW4/HW4/Controllers/MainViewController.swift
+++ HW4/HW4/Controllers/MainViewController.swift
@@ -11,7 +11,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.interactivePopGestureRecognizer?.delegate = nil
         // Do any additional setup after loading the view.
     }
     
---
+++ HW4/Pods/Target Support Files/Pods-HW4-HW4UITests/Pods-HW4-HW4UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HW4_HW4UITests : NSObject
+@end
+@implementation PodsDummy_Pods_HW4_HW4UITests
+@end
